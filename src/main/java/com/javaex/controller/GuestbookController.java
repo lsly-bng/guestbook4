5ed321e74f2ecb3e799abb5f9b71cbf0b19ca696
bhw <wyhoys92@gmail.com>
2022-06-16 16:19:14 +0900

    guestbook4 updated.

---
+++ src/main/java/com/javaex/controller/GuestbookController.java
@@ -0,0 +1,70 @@
+package com.javaex.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.javaex.service.GuestbookService;
+import com.javaex.vo.GuestbookVo;
+
+@Controller
+public class GuestbookController {
+
+	// field
+	@Autowired
+	private GuestbookService gbService;
+	// constructor
+	// method - g/s
+	// method - general
+
+	// addList (등록폼)
+	@RequestMapping(value = "/addList", method = { RequestMethod.GET, RequestMethod.POST })
+	public String addList(Model model) {
+
+		System.out.println("GuestbookController>addList()");
+
+		List<GuestbookVo> gbList = gbService.getGbList();
+
+		// DispatcherServlet에 데이터를 보냄
+		model.addAttribute("gbList", gbList);
+
+		return "addList";
+	}
+
+	// add (등록)
+	@RequestMapping(value = "/add", method = { RequestMethod.GET, RequestMethod.POST })
+	public String add(@ModelAttribute GuestbookVo gbVo) {
+
+		System.out.println("GuestbookController>add()");
+
+		gbService.gbInsert(gbVo);
+
+		return "redirect:/addList";
+	}
+
+	// deleteForm (삭제폼)
+	@RequestMapping(value = "/deleteForm", method = { RequestMethod.GET, RequestMethod.POST })
+	public String deleteForm() {
+
+		System.out.println("GuestbookController>deleteForm()");
+
+		return "deleteForm";
+	}
+
+	// delete (삭제)
+	@RequestMapping(value = "/delete", method = { RequestMethod.GET, RequestMethod.POST })
+	public String delete(@ModelAttribute GuestbookVo gbVo) {
+
+		System.out.println("GuestbookController>delete()");
+
+		gbService.gbDelete(gbVo);
+
+		return "redirect:/addList";
+	}
+
+}
---
+++ webapp/WEB-INF/views/deleteForm.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+
+<body>
+	<h1>[삭제]</h1>
+
+	<form action="/guestbook4/delete" method="get">
+		<table>
+			<tr>
+				<td>비밀번호</td>
+				<td><input type="password" name="password"></td>
+				<td><input type="submit" value="삭제"></td>
+			</tr>
+		</table>
+		<a href="/guestbook4/addList">메인으로 돌아가기</a><br> <input type="hidden"
+			name="no" value="${param.no}">
+	</form>
+
+</body>
+
+</html>
